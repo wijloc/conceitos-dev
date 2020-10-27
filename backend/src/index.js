76dfc534d7e26eb9a30a1da3be144de7c0e5a637
Willian Oliveira <wijloc@gmail.com>
2020-10-27 12:19:20 -0300

    feat: add routes get, post, put and delete

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-  return res.json({
-    nome: "willian",
-  })
+app.get('/projects', (req, res)=>{
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+  ])
+})
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+})
+
+app.put('/projects/:id', (req, res) => {  
+  return res.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+})
+
+app.delete('/projects/:id', (req, res) => {  
+  return res.json([    
+    'Projeto 2',
+    'Projeto 3',
+  ])
 })
 
 app.listen(3333, () => {
