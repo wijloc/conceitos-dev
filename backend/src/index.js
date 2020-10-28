ef8725b8f1cdc0b23f8b18ba535e2efbf974510e
Willian Oliveira <wijloc@gmail.com>
2020-10-28 10:13:51 -0300

    feat: using array as application memory

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,66 @@
 const express = require('express');
+const {uuid} = require('uuidv4')
 
 const app = express();
+app.use(express.json());
+
+const projects = [];
 
 app.get('/projects', (req, res)=>{
-  return res.json([
-    'Projeto 1',
-    'Projeto 2',
-  ])
+  const { title } = req.query;
+
+  const results = title
+  ?projects.filter(project =>project.title.includes(title))
+  :projects
+
+  return res.json(results)
 })
 
 app.post('/projects', (req, res) => {
-  return res.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const {title, owner} = req.body;
+
+  const project = {id:uuid(),title, owner};
+
+  projects.push(project);
+  
+  return res.json(project);
 })
 
 app.put('/projects/:id', (req, res) => {  
-  return res.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  console.log(projects)
+
+  const { id } = req.params;
+  const {title, owner} = req.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0){
+    return res.status(400).json({error: 'Project not found'})
+  }
+  
+  const project = {
+    id,
+    title, 
+    owner,
+  }
+
+  projects[projectIndex] = project;
+
+  return res.json(project)
 })
 
 app.delete('/projects/:id', (req, res) => {  
-  return res.json([    
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const { id } = req.params;
+  
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0){
+    return res.status(400).json({error: 'Project not found'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
 })
 
 app.listen(3333, () => {
