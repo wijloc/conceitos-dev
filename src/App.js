d60810c9218283d98b42fe783f22aaa6ab08b82e
Willian Oliveira <wijloc@gmail.com>
2020-10-29 11:19:29 -0300

    refactor: changes in header to use children data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
+
   return (
     <>
-      <Header/>
-      <Header/>
-      <Header/>
-      <Header/>
+      <Header title="Homepage">
+        <ul>
+          <li>Home</li>
+          <li>Projetos</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Home</li>
+          <li>Projetos</li>
+          <li>Sites</li>
+        </ul>
+      </Header>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export default function Header({title}){
+export default function Header({title, children}){
 
   return (
-    <h1>{title}</h1>
+    <header>
+      <h1>{title}</h1>
+      {children}
+    </header>    
   );
 }
