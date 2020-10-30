0786b499f037dec1565fb0a29dc907151bb94a46
Willian Oliveira <wijloc@gmail.com>
2020-10-30 10:49:17 -0300

    feat: getting values through api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import api from './services/api'
 import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const [projects, setProjects] = useState(['HTML', 'CSS', 'JavaScript']);  
+  const [projects, setProjects] = useState([]);  
+  
+  useEffect(()=>{
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  },[])
 
   function handleAddProject(){    
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -17,7 +23,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map((project) => <li key={project} > { project}</li>)}
+        {projects.map((project) => <li key={project.id} > { project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
