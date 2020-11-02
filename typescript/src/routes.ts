001d87a7f675f622592d7a3b4bddf7d2b563f2c0
Willian Oliveira <wijloc@gmail.com>
2020-11-02 16:45:37 -0300

    feat: define our own types

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,8 @@
 import {Request, Response } from 'express';
+import createUser from './services/CreateUser'
 
 export function helloWorld(request: Request, response: Response){
+  const user = createUser('Willian', 'wijloc@gmail.com', '12345679');
+
   return response.json({ message : 'Hello World'});  
 }
---
+++ typescript/src/services/CreateUser.ts
@@ -0,0 +1,11 @@
+
+
+export default function createUser(name = '', email: string, password: string){
+  const user = {
+    name,
+    email,
+    password,
+  }
+
+  return user;
+}
