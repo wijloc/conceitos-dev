5c31a716344feccc473206d1117be7e8c0c3e521
Willian Oliveira <wijloc@gmail.com>
2020-11-02 12:22:22 -0300

    feat: add new projects through api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api'
 
@@ -14,6 +14,16 @@ export default function App(){
     })
   }, [])
 
+  async function handleAddProject(){
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Willian Oliveira',
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
     <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
@@ -25,6 +35,14 @@ export default function App(){
           <Text style={styles.project}>{project.title}</Text>
         )}
       />
+
+      <TouchableOpacity 
+        activeOpacity={0.6}
+        style={styles.button}
+        onPress={handleAddProject}
+      >
+        <Text style= {styles.buttonText}>Adicionar projeto</Text>
+      </TouchableOpacity>
     </SafeAreaView>
     </>
   )
@@ -37,6 +55,20 @@ const styles = StyleSheet.create({
   },
   project: {
     color: '#fff',
-    fontSize:30,
+    fontSize:20,
+  },
+
+  button:{
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  buttonText:{
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 })
