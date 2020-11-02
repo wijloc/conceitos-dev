a2b7b5dc10d95db80e3800d4746c655933955b09
Willian Oliveira <wijloc@gmail.com>
2020-11-02 16:52:48 -0300

    feat: define our won types for objects

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -2,7 +2,12 @@ import {Request, Response } from 'express';
 import createUser from './services/CreateUser'
 
 export function helloWorld(request: Request, response: Response){
-  const user = createUser('Willian', 'wijloc@gmail.com', '12345679');
+  const user = createUser({
+    name: 'Willian',
+    email: 'wijloc@gmail.com',
+    password: '123456789',
+    techs: ['Node.js', 'ReactJS', 'React Native']
+  });    
 
   return response.json({ message : 'Hello World'});  
 }
--- typescript/src/services/CreateUser.ts
+++ typescript/src/services/CreateUser.ts
@@ -1,6 +1,12 @@
 
+interface CreateUserData{
+  name?: string,
+  email: string,
+  password: string,
+  techs: Array<string>
+}
 
-export default function createUser(name = '', email: string, password: string){
+export default function createUser({name = '', email, password}:CreateUserData){
   const user = {
     name,
     email,
